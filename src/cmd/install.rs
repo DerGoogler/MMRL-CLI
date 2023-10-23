6c2ef1dc72c364671d4420c926c01a896a19dfed
Der_Googler <54764558+DerGoogler@users.noreply.github.com>
2023-10-23 16:42:59 +0200

    Add install via git

--- src/cmd/install.rs
+++ src/cmd/install.rs
@@ -1,24 +1,37 @@
 extern crate serde;
 extern crate serde_ini;
 
-use crate::utils::{confirm, download_from_url, get_last, get_mmrl_json, is_url};
 use crate::android_root::{get_downloads_dir, get_install_cli};
 use crate::repo::{find_module, find_version, get_id_details, Module};
-use reqwest::Client;
+use crate::utils::{confirm, download_from_url, get_last, get_mmrl_json, is_git, is_url, zip_dir};
 use async_recursion::async_recursion;
+use git2::Repository;
+use reqwest::Client;
+use std::fs::{self, File};
 use std::io::{BufRead, BufReader, Error, ErrorKind};
 use std::path::Path;
 use std::process::{exit, Command, Stdio};
+use walkdir::WalkDir;
 
 #[async_recursion]
 async fn check_requires(
+    _is_git: bool,
     path: String,
     install_requires: bool,
     client: Client,
     yes: bool,
     modules: &Vec<Module>,
-) {
-    let mini = get_mmrl_json(&path);
+) -> () {
+    let mini: Result<crate::utils::MMRLJSON, serde_json::Error>;
+
+    if _is_git {
+        mini = match File::open(path) {
+            Ok(file) => serde_json::from_reader(file),
+            Err(..) => serde_json::from_str("{\"require\":[]}"),
+        };
+    } else {
+        mini = get_mmrl_json(&path);
+    }
 
     for req in mini.unwrap().require {
         let dep_path = Path::new("/data/adb/modules")
@@ -30,7 +43,7 @@ async fn check_requires(
         if !(dep_path_update.exists() || dep_path.exists()) {
             if install_requires {
                 println!("Install requires");
-                install(client.clone(), yes, install_requires,  modules, req).await;
+                install(client.clone(), yes, install_requires, modules, req).await;
             } else {
                 println!("This module requires {} to be installed", req.clone());
                 exit(1)
@@ -39,31 +52,82 @@ async fn check_requires(
     }
 }
 
+const METHOD_STORED: Option<zip::CompressionMethod> = Some(zip::CompressionMethod::Stored);
+
 #[async_recursion]
 pub async fn install(client: Client, yes: bool, requires: bool, modules: &Vec<Module>, id: String) {
     let _url = &id.to_owned()[..];
-    if !is_url(_url) {
-        let (_id, _ver) = get_id_details(id);
-        let module = find_module(&modules, _id.clone());
-        let version = find_version(module.versions.clone(), _ver);
+    if is_git(_url) {
+        let name = get_last(_url);
+        let path = &[get_downloads_dir(), name.unwrap()].join("/");
+        match Repository::clone(_url, path) {
+            Ok(repo) => repo,
+            Err(e) => panic!("failed to clone: {}", e),
+        };
 
+        check_requires(
+            true,
+            [path, "mmrl.json"].join("/"),
+            requires,
+            client.clone(),
+            yes,
+            modules,
+        )
+        .await;
+
+        let file = File::create([path, "zip"].join(".")).unwrap();
+
+        let walkdir = WalkDir::new(path);
+        let it = walkdir.into_iter();
+
+        zip_dir(
+            &mut it.filter_map(|e| e.ok()),
+            path,
+            file,
+            METHOD_STORED.unwrap(),
+        )
+        .unwrap();
+
+        if Path::new(path).exists() {
+            fs::remove_dir_all(path).expect("File delete failed");
+        }
+
+        println!("Info not availabe in git install");
+        let success = yes || confirm("Do you want to continue [y/N] ");
+
+        if success {
+            let (bin, args) = get_install_cli(&path);
+
+            let stdout = Command::new(bin)
+                .args(args)
+                .stdout(Stdio::piped())
+                .spawn()
+                .unwrap()
+                .stdout
+                .ok_or_else(|| Error::new(ErrorKind::Other, "Could not capture standard output."))
+                .unwrap();
+
+            let reader = BufReader::new(stdout);
+
+            reader
+                .lines()
+                .filter_map(|line| line.ok())
+                .for_each(|line| println!("{}", line));
+        } else {
+            exit(0);
+        }
+    } else if is_url(_url) {
+        let name = get_last(_url);
         let path = &[
             get_downloads_dir(),
-            [
-                [version.version.clone(), module.id].join("-"),
-                "zip".to_string(),
-            ]
-            .join("."),
+            [name.clone().unwrap().to_string(), "zip".to_string()].join("."),
         ]
         .join("/");
+        download_from_url(client.clone(), id.clone(), name.unwrap(), path).await;
+        check_requires(false, path.clone(), requires, client.clone(), yes, modules).await;
 
-        println!("Downloading {}", module.name);
-        println!("Version: {}", &version.version);
-
-        download_from_url(client.clone(), version.zip_url, module.name, path).await;
-        check_requires(path.clone(), requires, client.clone(), yes, modules).await;
-
-        let success = yes || confirm("Do you want to continue [y/N]? ");
+        println!("Info not availabe in url install");
+        let success = yes || confirm("Do you want to continue [y/N] ");
 
         if success {
             let (bin, args) = get_install_cli(&path);
@@ -87,17 +151,27 @@ pub async fn install(client: Client, yes: bool, requires: bool, modules: &Vec<Mo
             exit(0);
         }
     } else {
-        let name = get_last(_url);
+        let (_id, _ver) = get_id_details(id);
+        let module = find_module(&modules, _id.clone());
+        let version = find_version(module.versions.clone(), _ver);
+
         let path = &[
             get_downloads_dir(),
-            [name.clone().unwrap().to_string(), "zip".to_string()].join("."),
+            [
+                [version.version.clone(), module.id].join("-"),
+                "zip".to_string(),
+            ]
+            .join("."),
         ]
         .join("/");
-        download_from_url(client.clone(), id.clone(), name.unwrap(), path).await;
-        check_requires(path.clone(), requires, client.clone(), yes, modules).await;
 
-        println!("Info not availabe in url install");
-        let success = yes || confirm("\nDo you want to continue [y/N]? ");
+        println!("Downloading {}", module.name);
+        println!("Version: {}", &version.version);
+
+        download_from_url(client.clone(), version.zip_url, module.name, path).await;
+        check_requires(false, path.clone(), requires, client.clone(), yes, modules).await;
+
+        let success = yes || confirm("Do you want to continue [y/N] ");
 
         if success {
             let (bin, args) = get_install_cli(&path);
--- src/utils.rs
+++ src/utils.rs
@@ -1,15 +1,24 @@
+extern crate walkdir;
+extern crate zip;
+
 use futures_util::StreamExt;
 use indicatif::{ProgressBar, ProgressStyle};
 use regex::Regex;
 use reqwest::Client;
-use url::Url;
+use serde::{Deserialize, Serialize};
 use std::cmp::min;
 use std::fs::File;
 use std::io;
 use std::io::prelude::*;
 use std::path::Path;
+use url::Url;
 use zip::ZipArchive;
-use serde::{Deserialize, Serialize};
+
+use std::io::{Seek, Write};
+use std::iter::Iterator;
+use zip::write::FileOptions;
+
+use walkdir::DirEntry;
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
@@ -27,7 +36,7 @@ pub fn get_mmrl_json(path: &str) -> Result<MMRLJSON, serde_json::Error> {
         Ok(file) => file,
         Err(..) => {
             println!("mmrl.json not found");
-            return serde_json::from_str("{\"require\":[]}")
+            return serde_json::from_str("{\"require\":[]}");
         }
     };
 
@@ -114,4 +123,50 @@ pub async fn download_from_url(client: Client, url: String, name: String, path: 
 pub fn is_url(url: &str) -> bool {
     let url_regex: &str = r"https?://(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}(\.[a-z]{2,4})?\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)";
     return Regex::new(url_regex).unwrap().is_match(url);
+}
+
+pub fn is_git(url: &str) -> bool {
+    let git_regex: &str =
+        r"((git|ssh|http(s)?)|(git@[\w\.]+))(:(\/\/)?)([\w\.@\:\/\-~]+)(\.git)(\/)?";
+    return Regex::new(git_regex).unwrap().is_match(url);
+}
+
+pub fn zip_dir<T>(
+    it: &mut dyn Iterator<Item = DirEntry>,
+    prefix: &str,
+    writer: T,
+    method: zip::CompressionMethod,
+) -> zip::result::ZipResult<()>
+where
+    T: Write + Seek,
+{
+    let mut zip = zip::ZipWriter::new(writer);
+    let options = FileOptions::default()
+        .compression_method(method)
+        .unix_permissions(0o755);
+
+    let mut buffer = Vec::new();
+    for entry in it {
+        let path = entry.path();
+        let name = path.strip_prefix(Path::new(prefix)).unwrap();
+
+        // Write file or directory explicitly
+        // Some unzip tools unzip files with directory paths correctly, some do not!
+        if path.is_file() {
+            println!("adding file {:?} as {:?} ...", path, name);
+            zip.start_file_from_path(name, options)?;
+            let mut f = File::open(path)?;
+
+            f.read_to_end(&mut buffer)?;
+            zip.write_all(&*buffer)?;
+            buffer.clear();
+        } else if name.as_os_str().len() != 0 {
+            // Only if not root! Avoids path spec / warning
+            // and mapname conversion failed error on unzip
+            println!("adding dir {:?} as {:?} ...", path, name);
+            zip.add_directory_from_path(name, options)?;
+        }
+    }
+    zip.finish()?;
+    Result::Ok(())
 }
