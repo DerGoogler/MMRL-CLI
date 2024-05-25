30c229b12419e876228c5bdb8cfa1c77fad9f530
Der_Googler <54764558+DerGoogler@users.noreply.github.com>
2024-05-25 19:01:10 +0200

    0.3.3

--- build-module.sh
+++ build-module.sh
@@ -7,6 +7,7 @@ get_prop() { cat ./../Cargo.toml | grep -Po "(?<=^$1 = \")[^\"]*(?=\".*)"; }
 
 NAME=$(get_prop name)
 VER=$(get_prop version)
+AUT=$(get_prop author)
 VER_CODE="${VER//./}"
 BUILD_DATE=`date "+%Y-%m-%d"`
 RUST_VER=$(rustc --version | grep -oP '\d+\.\d+\.\d+')
@@ -17,7 +18,7 @@ id=$NAME
 name=MMRL CLI
 version=$VER
 versionCode=$VER_CODE
-author=Der_Googler
+author=$AUT
 description=MMRL Command Line Interface is a free tool to install Magisk/KernelSU modules. Build on $BUILD_DATE with Rust $RUST_VER.
 updateJson=https://raw.githubusercontent.com/DerGoogler/MMRL-CLI/master/module/update.json
 EOF
@@ -27,8 +28,18 @@ cat << EOF >update.json
 {
     "version": "$VER",
     "versionCode": "$VER_CODE",
-    "zipUrl": "https://github.com/DerGoogler/MMRL-CLI/releases/download/v$VER/mmrl-$VER_CODE-module-aarch64.zip",
+    "zipUrl": "https://github.com/DerGoogler/MMRL-CLI/releases/download/v$VER/mmrl-$VER-module-aarch64.zip",
     "changelog": "https://raw.githubusercontent.com/DerGoogler/MMRL-CLI/master/CHANGELOG.md"
+}
+EOF
+
+cat << EOF >system/usr/share/mmrl/mmrl/info.json
+{
+    "version": "$VER",
+    "versionCode": "$VER_CODE",
+    "author": "$AUT",
+    "rustVersion": "$RUST_VER",
+    "buildDate": "$BUILD_DATE"
 }
 EOF
 
