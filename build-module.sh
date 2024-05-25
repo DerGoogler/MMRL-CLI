8ff72f77375e9bcaaeb5f75acf9a45075e2efa28
Der_Googler <54764558+DerGoogler@users.noreply.github.com>
2024-05-25 13:24:18 +0200

    update build script

--- build-module.sh
+++ build-module.sh
@@ -11,20 +11,32 @@ VER_CODE="${VER//./}"
 BUILD_DATE=`date "+%Y-%m-%d"`
 RUST_VER=$(rustc --version | grep -oP '\d+\.\d+\.\d+')
 
-arr=(
-    "id=$NAME"
-    "name=MMRL CLI"
-    "version=$VER"
-    "versionCode=$VER_CODE"
-    "author=Der_Googler"
-    "description=MMRL Command Line Interface is a free tool to install Magisk/KernelSU modules. Build on $BUILD_DATE with Rust $RUST_VER."
-);
-printf '%s\n' "${arr[@]}" > module.prop
+# generate module.prop
+cat << EOF >module.prop
+id=$NAME
+name=MMRL CLI
+version=$VER
+versionCode=$VER_CODE
+author=Der_Googler
+description=MMRL Command Line Interface is a free tool to install Magisk/KernelSU modules. Build on $BUILD_DATE with Rust $RUST_VER.
+updateJson=https://raw.githubusercontent.com/DerGoogler/MMRL-CLI/master/module/update.json
+EOF
 
-cp ./../target/aarch64-linux-android/release/mmrl system/bin/mmrl
+# generate update.json
+cat << EOF >update.json
+{
+    "version": "$VER",
+    "versionCode": "$VER_CODE",
+    "zipUrl": "https://github.com/DerGoogler/MMRL-CLI/releases/download/v$VER/mmrl-$VER_CODE-module-aarch64.zip",
+    "changelog": "https://raw.githubusercontent.com/DerGoogler/MMRL-CLI/master/CHANGELOG.md"
+}
+EOF
+
+cp ../CHANGELOG.md .
+cp ../target/aarch64-linux-android/release/mmrl system/bin/mmrl
 
 FILE_NAME="$NAME-$VER-module-aarch64.zip"
 
-zip -r "./../target/$FILE_NAME" ./*
+zip -r "../target/$FILE_NAME" ./* -x "system/bin/placeholder"
 
-rm -rf system/bin/mmrl
+rm -rf "system/bin/mmrl" "CHANGELOG.md"
--- module/install.sh
+++ module/install.sh
@@ -1,11 +1,3 @@
-SKIPMOUNT=false
-PROPFILE=false
-POSTFSDATA=false
-LATESTARTSERVICE=false
-
-REPLACE=""
-
-
 print_modname() {
     ui_print "    __  _____  _______  __ "
     ui_print "   /  |/  /  |/  / __ \/ / "
@@ -18,10 +10,17 @@ on_install() {
     ui_print "- Extracting module files"
     unzip -qq -o "$ZIPFILE" 'system/*' -d $MODPATH >&2
     [ -d "$MODPATH/system/bin/" ] || mkdir -p "$MODPATH/system/bin/"
+    ln -s "$MODPATH/system/bin/mmrl" "$MODPATH/system/bin/mrepo"
+    ln -s "$MODPATH/system/bin/mmrl" "$MODPATH/system/bin/foxmmm"
+    ln -s "$MODPATH/system/bin/mmrl" "$MODPATH/system/bin/amm"
+
 }
 
 set_permissions() {
     # The following is the default rule, DO NOT remove
-    set_perm_recursive $MODPATH 0 0 0755 0644
-    set_perm $MODPATH/system/bin/mmrl 0 0 0755
+    set_perm_recursive "$MODPATH" 0 0 0755 0644
+    set_perm "$MODPATH/system/bin/mmrl" 0 0 0755
+    set_perm "$MODPATH/system/bin/mrepo" 0 0 0755
+    set_perm "$MODPATH/system/bin/foxmmm" 0 0 0755
+    set_perm "$MODPATH/system/bin/amm" 0 0 0755
 }
