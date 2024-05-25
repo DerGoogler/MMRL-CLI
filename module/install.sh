211e8aaea8cf9ac3bc9758aabc2214f7fb651191
Der_Googler <54764558+DerGoogler@users.noreply.github.com>
2024-05-25 20:16:00 +0200

    .

--- module/install.sh
+++ module/install.sh
@@ -15,17 +15,17 @@ on_install() {
     ui_print "- Extracting module files"
     unzip -qq -o "$ZIPFILE" 'system/*' -d $MODPATH >&2
     [ -d "$MODPATH/system/bin/" ] || mkdir -p "$MODPATH/system/bin/"
-    ln -s "$MODPATH/system/bin/mmrl" "$MODPATH/system/bin/mrepo"
-    ln -s "$MODPATH/system/bin/mmrl" "$MODPATH/system/bin/foxmmm"
-    ln -s "$MODPATH/system/bin/mmrl" "$MODPATH/system/bin/amm"
+    ln -sf mmrl $MODPATH/system/bin/mrepo
+    ln -sf mmrl $MODPATH/system/bin/foxmmm
+    ln -sf mmrl $MODPATH/system/bin/amm
 
 }
 
 set_permissions() {
     # The following is the default rule, DO NOT remove
     set_perm_recursive $MODPATH 0 0 0755 0644
-    set_perm "$MODPATH/system/bin/mmrl" 0 0 0755
-    set_perm "$MODPATH/system/bin/mrepo" 0 0 0755
-    set_perm "$MODPATH/system/bin/foxmmm" 0 0 0755
-    set_perm "$MODPATH/system/bin/amm" 0 0 0755
+    set_perm $MODPATH/system/bin/mmrl 0 0 0755
+    set_perm $MODPATH/system/bin/mrepo 0 0 0755
+    set_perm $MODPATH/system/bin/foxmmm 0 0 0755
+    set_perm $MODPATH/system/bin/amm 0 0 0755
 }
